f4c42a2b8f8ecb68c60eb335203450faf77d53db
Josh Long <josh@joshlong.com>
2020-06-07 05:42:13 -0700

    deploy

--- publish.sh
+++ publish.sh
@@ -7,6 +7,7 @@ rm -rf build
 rm -rf simple_python_github_client.egg-info
 
 pipenv install
+pipenv install twine
 pipenv run python setup.py sdist bdist_wheel
 pipenv run twine check dist/*
 pipenv run twine upload --verbose --non-interactive --repository-url ${REPOSITORY_URL} -u "$ARTIFACTORY_USERNAME" -p "$ARTIFACTORY_PASSWORD" dist/*
